a288d69cdf8260633eb1bbf56739f16cb07c7691
James Kim <yt4921@gmail.com>
2024-12-23 16:22:28 +0900

    Create the switch function

--- ClassForUniversity/ClassForUniversity/ClassForUniversity.cpp
+++ ClassForUniversity/ClassForUniversity/ClassForUniversity.cpp
@@ -1,6 +1,7 @@
 # include <iostream>
 # include <vector>
 # include <string>
+#include <algorithm>
 
 
 using namespace std;
@@ -61,6 +62,20 @@ public:
 		courses.push_back(course);
 	}
 
+	void swap(string originalCourse, string courseToSwitch, int numberOfCourses) {
+		if (find(courses.begin(), courses.end(), originalCourse) == courses.end()) {
+			cout << "The course you want to switch is not in your course cart. " << endl;
+		}
+
+		for (int i = 0; i < numberOfCourses; i++) {
+			if (courses[i] == originalCourse) {
+				courses[i] = courseToSwitch;
+				cout << "The course has successfully swapped. Now your courses are ";
+
+			}
+		}
+	}
+
 	void printStudent() {
 		cout << "Name: " << this->name << endl;
 		cout << "Student number: " << this->studentNumber << endl;
@@ -87,14 +102,14 @@ public:
 	~Student() {}
 };
 
-class UnderGraduate :Student {
-private:
-	string major;
-public:
-	UnderGraduate(string major) {
-		this->major = major;
-	}
-};
+//class UnderGraduate :Student {
+//private:
+//	string major;
+//public:
+//	UnderGraduate(string major) {
+//		this->major = major;
+//	}
+//};
 
 int main() {
 	int numberOfCourses = 0;
@@ -109,10 +124,35 @@ int main() {
 		s1.enroll(courseName);
 	}
 	cout << endl;
-
-	UnderGraduate g1("math");
+	//UnderGraduate g1("math");
 
 	s1.printStudent();
+	int userChoice = 0;
+	cout << endl << "Which service do you want to take? " << endl;
+	cout << "Option 1: Swap" << endl << "Option 2: Drop" << endl << "Option 3: End the system" << endl
+		<< "Please choose a number between 1-3: ";
+	cin >> userChoice;
+	if (userChoice == 1) {
+		string originalCourse;
+		string courseToSwitch;
+		cout << "Thank you to choose the SWAP service. " << endl;
+		cout << "Please tell me the course you want to switch from your course cart: ";
+		getline(cin >> ws, originalCourse);
+		cout << "Please tell me the course you want to replace ";
+		getline(cin >> ws, courseToSwitch);
+		s1.swap(originalCourse, courseToSwitch, numberOfCourses);
+		s1.printStudent();
+	}
+	else if (userChoice == 2) {
+		string originalCourse;
+		cout << "Thank you to choose the DROP service. " << endl;
+		cout << "Please tell me the course you want to drop: ";
+		getline(cin >> ws, originalCourse);
+	}
+	else if (userChoice == 3) {
+		return 0;
+	}
+
 	//s1.setStudentMail("jam123@sfu.ca");
 	//cout << "After set" << endl;
 	//s1.printStudent();
